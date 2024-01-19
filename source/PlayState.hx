18b352cdde5807fbd3dbeeb1e344128232f69923
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2024-01-19 14:54:41 -0500

    haxeflixel init

---
+++ source/PlayState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
