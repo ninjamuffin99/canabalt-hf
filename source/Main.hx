2c1c24108893d22349022cc6a3065c83fb6f0c08
George FunBook <gkurelic@gmail.com>
2024-02-13 09:55:10 -0600

    use default splash

--- source/Main.hx
+++ source/Main.hx
@@ -1,14 +1,16 @@
 package;
 
-import openfl.Lib;
-import flixel.input.keyboard.FlxKey;
-import flixel.input.keyboard.FlxKeyList;
-import openfl.events.KeyboardEvent;
-import lime.app.Event;
-import lime.app.Application;
 import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.input.keyboard.FlxKey;
+import flixel.input.keyboard.FlxKeyList;
+import flixel.system.FlxSplash;
+import lime.app.Event;
+import lime.app.Application;
+import openfl.Lib;
+import openfl.events.KeyboardEvent;
 import openfl.display.Sprite;
+
 
 class Main extends Sprite {
 	// Why can't Safari play .ogg in 2024.... :(
@@ -28,12 +30,26 @@ class Main extends Sprite {
 		Application.current.window.element.style.setProperty("image-rendering", "pixelated");
 		#end
 
-		addChild(new FlxGame(480, 160, FlxSplash, framerate, framerate, true));
+		addChild(new FlxGame(480, 160, ()->new Splash(MenuState.new), framerate, framerate, true));
 
 		Lib.current.stage.addEventListener(openfl.events.KeyboardEvent.KEY_DOWN, (e:KeyboardEvent) -> {
 			if (e.keyCode == FlxKey.F) {
 				Lib.application.window.fullscreen = !Lib.application.window.fullscreen;
 			}
 		});
+	}
+}
+
+class Splash extends FlxSplash
+{
+	override function create()
+	{
+		// set bgColor before and after `super.create()` otherwise it will set it to black for a frame
+		FlxG.cameras.bgColor = 0xff35353d;
+		FlxSplash.muted = false;
+		
+		super.create();
+		
+		FlxG.cameras.bgColor = 0xff35353d;
 	}
 }
